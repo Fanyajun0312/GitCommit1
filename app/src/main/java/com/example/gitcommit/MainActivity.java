e692b65a4f49fc323e55b585104b91a35786d2eb
e <472741357@qq.com>
2020-07-10 15:26:03 +0800

    范亚军添加代码上传

---
+++ app/src/main/java/com/example/gitcommit/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.gitcommit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String fyj="范亚军真帅";
+    }
+}
